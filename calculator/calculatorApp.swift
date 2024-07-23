6437d9e22c9c5ee82dd93dfc18f69fcb50de5808
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-07-23 12:57:38 -0400

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Ben Landrette on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
